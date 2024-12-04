04124c07c356cd4f16a93d3fa8678b78b837c415
umeshdupade-wtw <136604437+umeshdupade-wtw@users.noreply.github.com>
2024-12-04 08:46:32 +0000

    St.Barnabas rates query

---
+++ St_Barnabas_2025_rates_update_counts.sql
@@ -0,0 +1,12 @@
+
+select count(*) from master_policy;
+select count(*) from master_policy_coverage;
+select count(*) from r_assessment;
+select count(*) from r_assessment_req_var;
+select count(*) from r_rate_page;
+select count(*) from r_rate_page_assessment_link;
+select count(*) from hospital_mpc_link;
+select count(*) from r_rate;
+select count(*) from r_rate_premium;
+select count(*) from r_rate_variable_value_map;
+
