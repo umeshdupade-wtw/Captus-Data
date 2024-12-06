34c8b2d5d21e86e6221240fc85e6ea6193716c3b
umeshdupade-wtw <136604437+umeshdupade-wtw@users.noreply.github.com>
2024-12-06 13:32:08 +0000

    Add files via upload
    Updated st.Barnabas rates query and count query

---
+++ St_Barnabas_2025_rates_update_counts_v1.sql
@@ -0,0 +1,14 @@
+select count(*) from r_rate_variable;
+select count(*) from R_RATE_VARIABLE_GROUP_VALUE;
+select count(*) from R_RATE_VARIABLE_ITEM_VALUE;
+select count(*) from master_policy;
+select count(*) from master_policy_coverage;
+select count(*) from r_assessment;
+select count(*) from r_assessment_req_var;
+select count(*) from r_rate_page;
+select count(*) from r_rate_page_assessment_link;
+select count(*) from hospital_mpc_link;
+select count(*) from r_rate;
+select count(*) from r_rate_premium;
+select count(*) from r_rate_variable_value_map;
+
