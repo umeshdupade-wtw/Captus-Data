668b46b2e1edbaf15fbe8d0e5f00624a0fcc4fc6
umeshdupade-wtw <136604437+umeshdupade-wtw@users.noreply.github.com>
2024-12-09 09:37:11 +0000

    Add files via upload
    Captus PROD release

---
+++ PIH_2025_Rates_update_counts.sql
@@ -0,0 +1,13 @@
+
+select count(*) from r_assessment;
+select count(*) from r_assessment_req_var;
+select count(*) from master_policy;
+select count(*) from master_policy_coverage;
+select count(*) from r_rate_page;
+select count(*) from r_rate_page_assessment_link;
+select count(*) from hospital_mpc_link;
+select count(*) from r_rate;
+select count(*) from r_rate_premium;
+select count(*) from r_rate_variable_value_map;
+select count(*) from r_assessment_multiplier_var;
+select count(*) from r_multiplier_value;
